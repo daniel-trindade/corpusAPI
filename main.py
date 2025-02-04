ceba4697461f3ebdc961e4ddef4df1060aa06d2e
Onifin <iann909041@gmail.com>
2025-02-04 16:13:20 -0300

    translate route and languagelist route

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 from fastapi import FastAPI
 
-from video_transcript import get_video_transcript
+from video_transcript import get_video_transcript, list_languages, translate_video_transcript
 
 app = FastAPI()
 
@@ -11,7 +11,17 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/video/{id_video}")
+@app.get("/video")
+async def read_item(id_video: str, language_code: str):
+    transcript = get_video_transcript(id_video, language_code)
+    return {"video": transcript}
+
+@app.get("/video/language_list")
 async def read_item(id_video: str):
-    transcript = get_video_transcript(id_video)
+    transcript_list = list_languages(id_video)
+    return {"video": transcript_list}
+
+@app.get("/video/translate")
+async def read_item(id_video: str, source_language_code: str, target_language_code: str):
+    transcript = translate_video_transcript(id_video, source_language_code, target_language_code)
     return {"video": transcript}
--- video_transcript.py
+++ video_transcript.py
@@ -1,11 +1,42 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import TextFormatter
+    
+def list_languages(video_id):
+    try:
+        transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
 
-def get_video_transcript(video_id):
+        language_list = []
 
-    transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['pt'])
+        for transcript in transcript_list:
+            language_list.append(
+                {
+                    "language": transcript.language,
+                    "language_code": transcript.language_code
+                }
+            )
+
+        return language_list
+    except:
+        return []
+    
+def get_video_transcript(video_id, language):
+    
+    transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=[language])
 
     formatter = TextFormatter()
     text_formatted = formatter.format_transcript(transcript)
+
+    return text_formatted
+
+def translate_video_transcript(video_id, source_language_code, target_language_code):
+    transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+
+    transcript = transcript_list.find_transcript([source_language_code])
+    translated_transcript = transcript.translate(target_language_code)
+
+    translated_transcript = translated_transcript.fetch() #transforma em diciionario
+
+    formatter = TextFormatter()
+    text_formatted = formatter.format_transcript(translated_transcript)
 
     return text_formatted
