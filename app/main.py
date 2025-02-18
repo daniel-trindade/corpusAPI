06d4726575d366a7382487d999133a1bf2968d17
Onifin <iann909041@gmail.com>
2025-02-18 14:17:34 -0300

    cors error

--- app/main.py
+++ app/main.py
@@ -4,7 +4,17 @@
 from fastapi import FastAPI
 from app.routers import video, web, audio
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(video.router)
 app.include_router(web.router)
